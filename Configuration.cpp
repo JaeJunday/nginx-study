d9acb1f4dca9ae8ea1b2774581cabe21bdb41ca3
JaeJunday <wowns8114@gmail.com>
2023-07-14 13:33:54 +0900

    Feat: 괄호 짝 pop 함수 구현

--- Configuration.cpp
+++ Configuration.cpp
@@ -1,6 +1,41 @@
 #include "Configuration.hpp"
 #include <sstream> // 헤더 추가
 
+// OCF
+
+Configuration::Configuration() : _locationFlag(false), _serverFlag(false), _count(0)
+{
+    // Default Constructor Implementation
+}
+
+Configuration::~Configuration()
+{
+    // Destructor Implementation
+}
+
+Configuration::Configuration(const Configuration& other)
+    : _locationFlag(other._locationFlag),
+      _serverFlag(other._serverFlag),
+      _count(other._count),
+      _parenticts(other._parenticts)
+{
+    // Copy Constructor Implementation
+}
+
+Configuration& Configuration::operator=(const Configuration& other)
+{
+    if (this != &other) {
+        _locationFlag = other._locationFlag;
+        _serverFlag = other._serverFlag;
+        _count = other._count;
+        _parenticts = other._parenticts;
+    }
+    // Assignment Operator Implementation
+    return *this;
+}
+
+//==============================================================================
+
 void Configuration::parsing(const std::string& filePath)
 {
     std::ifstream	file;
@@ -19,14 +54,71 @@ void Configuration::parsing(const std::string& filePath)
         {
             if (word.empty() == true)
                 break;
-            else if (word == '{')
-                setStack(push);
-            else if (word == '}')
-                setStack(pop);
+            // refactoring 예정
+            else if (word == "server")
+            {
+                if (_serverFlag == true)
+                    throw std::runtime_error("Error: Server is already exist");
+                else if (_locationFlag == true)
+                    throw std::runtime_error("Error: Location is already exist");
+                else
+                {
+                    _serverFlag = true;
+                    ++_count;
+                    push("server");
+                }
+            }
+            else if (word == "location")
+            {
+                if (_locationFlag == true)
+                    throw std::runtime_error("Error: Location is already exist");
+                else if (_serverFlag == false)
+                    throw std::runtime_error("Error: Server is not exist");
+                else
+                {
+                    _locationFlag = true;
+                    ++_count;
+                    push("location");
+                }
+            }
+            else if (word == "{")
+                push("{"); 
+            else if (word == "}")
+            {
+                pop();
+            }
             // 이제 단어를 사용하여 필요한 작업을 수행할 수 있습니다.
             // 예를 들어, 단어를 벡터에 저장하거나 다른 처리를 할 수 있습니다.
         }
     }
     file.close();
+}
+
+void Configuration::pop() 
+{
+    std::string str;
+
+    if (_parenticts.empty() == true)
+        throw std::logic_error("Error: } is not pair");
+    // first pop
+    str = _parenticts.top();
+    if (str != "{")
+        throw std::logic_error("Error: { is not exist");
+    _parenticts.pop();
+    // second pop
+    str = _parenticts.top();
+    if (str != "server" && str != "location")
+        throw std::logic_error("Error: server or location is not exist");
+    if (str == "server")
+        _serverFlag = false;
+    else if (str == "location")
+        _locationFlag = false;
+    _count -= 1;
+    _parenticts.pop();
+}
+
+void Configuration::push(const std::string& input) 
+{
+
 }
 
--- Configuration.hpp
+++ Configuration.hpp
@@ -9,11 +9,19 @@
 class Configuration
 {
 private:
-    std::stack<std::string> _parenticts;
+	std::stack<std::string> _parenticts;
+	bool                    _locationFlag;
+	bool                    _serverFlag;
+	unsigned int            _count;
 public:
-    void parsing(const std::string& filePath);
-    void pushStack(const std::string& input);
-    void popStack();
+	Configuration(); // Default Constructor
+	~Configuration(); // Destructor
+	Configuration(const Configuration& other); // Copy Constructor
+	Configuration& operator=(const Configuration& other); // Assignment Operator
+
+	void parsing(const std::string& filePath);
+	void push(const std::string& input);
+	void pop();
 };
 
 // exception 
@@ -21,15 +29,15 @@ public:
 // 열린괄호 넣으면 열린괄호 넣음
 // 닫힌괄호 받으면 열린괄호 뺌
 
-//플래그
+// 플래그
 // 서버, 로케이션, 카운트
 // 의사코드
-//서버를 넣을떄 : 서버를 만나면 서버플래그랑 로케이션플래그 검사한 후 서버 스트링을 스택에 넣고 카운트를 올린다. 플래그를 서버로 바꾼다
-//로케이션을 넣을때 : 로케이션을 만나면 서버플래그랑 로케이션플래그를 검사한 후 로케이션 스트링을 스택에 넣고 카운트를 올린다 플래그를 로케이션으로 바꾼다
-//열린괄호를 만나면 열린괄호를 스택에 넣는다.
-//닫힌괄호를 만나면 열린괄호 - 스트링 순서를 맞추어서 스택에서 뺀다. 카운트를 내린다. 플래그를 끈다. 
-//다 읽었을때 카운트가 남아 있으면 에러
-//카운트가 0일때 스택이 비어있지 않으면 에러
+// 서버를 넣을떄 : 서버를 만나면 서버플래그랑 로케이션플래그 검사한 후 서버 스트링을 스택에 넣고 카운트를 올린다. 플래그를 서버로 바꾼다
+// 로케이션을 넣을때 : 로케이션을 만나면 서버플래그랑 로케이션플래그를 검사한 후 로케이션 스트링을 스택에 넣고 카운트를 올린다 플래그를 로케이션으로 바꾼다
+// 열린괄호를 만나면 열린괄호를 스택에 넣는다.
+// 닫힌괄호를 만나면 열린괄호 - 스트링 순서를 맞추어서 스택에서 뺀다. 카운트를 내린다. 플래그를 끈다. 
+// 다 읽었을때 카운트가 남아 있으면 에러
+// 카운트가 0일때 스택이 비어있지 않으면 에러
 
 // ;룰 끝 
 //find로 ;을 찾는다. 못찾으면 에러
@@ -40,3 +48,6 @@ public:
 
 //로케이션 안에 로케이션 로케이션이 꺼져있는지 
 //서버 밖에 로케이션 서버가 꺼져있는지 
+
+// server 조건식인 경우
+// { 시작전에 문자열이 들어온다 -> error
