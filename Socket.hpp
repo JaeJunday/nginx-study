5d0154f9b4be55248ed33916d39b0b97d4e42664
JaeJunday <wowns8114@gmail.com>
2023-07-21 10:41:02 +0900

    Remove: socket event struct variable remove

--- Socket.hpp
+++ Socket.hpp
@@ -2,16 +2,13 @@
 #include <sys/types.h>  // socket, bind
 #include <sys/socket.h> // socket
 #include <netinet/in.h> // sockaddr_in
-#include <sys/event.h>  // kqueue
 #include <fcntl.h>      // fcntl
 
 #define MAX_EVENTS 10
 
 struct Socket
 {
-	int server_fd, client_fd;
-	int kq, nev;
+	int serverFd, clientFd;
 	sockaddr_in		server_addr, client_addr;
 	socklen_t		client_len;
-	struct kevent	event, events[MAX_EVENTS];
 };
