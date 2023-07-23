7d01a06a8ebd9e8dc2733dc427e0666b3c3fa651
JaeJunday <wowns8114@gmail.com>
2023-07-23 17:08:09 +0900

    Feat: Request header cpp add

--- Request.cpp
+++ Request.cpp
@@ -1,26 +1,41 @@
 #include "Request.hpp"
 
-Request::Request()
+Request::Request(int socket)
+    : _socket(socket)
 {
 }
 
-Request::~Request()
-{
-}
+// Request::~Request()
+// {
+// }
 
-void Request::checkHeader() const
-{ 
+// void Request::checkHeader() const
+// { 
 
-}
+// }
 
-void Request::parsing()
-{
-    try {
-        checkHeader();
-        setOther();
-        makeMain();
-    } catch (std::exception &e) {
+// void Request::parsing()
+// {
+//     try {
+        // checkHeader();
+        // setOther();
+        // makeMain();
+//     } catch (std::exception &e) {
     
-    }
+//     }
+// }
+
+int Request::getSocket() const
+{
+    return _socket;
 }
 
+void Request::setBuffer(char* buffer)
+{
+    _buffer = buffer;
+}
+
+const char* Request::getBuffer() const
+{
+    return _buffer;
+}
--- Request.hpp
+++ Request.hpp
@@ -2,16 +2,23 @@
 
 #include <iostream>
 #include <string>
+#include "sys/socket.h"
+#include <netinet/in.h> // sockaddr_in
 
 class Request
 {
     private:
-        char buffer[];
+        int         _socket;
+        char*       _buffer;
         std::string _method;
         std::string _host;
         std::string _contentType;
         std::string _contentLength;
     public:
-        void Request::parsing();
-        void Request::checkHeader() const
+        Request(int socket);
+        // void Request::parsing();
+        void setSocket(int fd);
+        int getSocket() const;
+        const char* getBuffer() const;
+        void setBuffer(char* buffer);
 };
