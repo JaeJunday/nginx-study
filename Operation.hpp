fb060a4d454d33c105ebe1712169c32b895290cf
JaeJunday <wowns8114@gmail.com>
2023-07-23 17:07:50 +0900

    Feat: Operation add Request list

--- Operation.hpp
+++ Operation.hpp
@@ -2,21 +2,25 @@
 
 #include "Server.hpp"
 #include "Socket.hpp"
+#include "Request.hpp"
 #include <iostream>
 #include <string>
 #include <vector>
+#include <list>
 #include <unistd.h>
 #include <sys/event.h>  // kqueue
 
 #define FALLOW 0
 
+typedef std::list<Request>::iterator ITOR;
+
 class Operation {
 private:
     std::vector<Server> _servers;
+    std::list<Request> _requests;
 public:
     void setServer(const Server& server);
     const std::vector<Server>& getServers() const;
     int createBoundSocket(int port);
     void start();
-    void timeStamp() const;
 };
