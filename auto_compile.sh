edc5be0bcc7bf24b5c9ea99ef9584ab1235a5f93
JaeJunday <wowns8114@gmail.com>
2023-07-29 17:34:31 +0900

    Feat: color setting add

--- auto_compile.sh
+++ auto_compile.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# ANSI escape 코드를 사용하여 터미널 출력 색상 및 스타일 설정
+Black='\033[0;30m'
+Red='\033[0;31m'
+Green='\033[0;32m'
+Yellow='\033[0;33m'
+Blue='\033[0;34m'
+Purple='\033[0;35m'
+Cyan='\033[0;36m'
+White='\033[0;37m'
+B_Black='\033[0;90m'
+B_Red='\033[0;91m'
+B_Green='\033[0;92m'
+B_Yellow='\033[0;93m'
+B_Blue='\033[0;94m'
+B_Purple='\033[0;95m'
+B_Cyan='\033[0;96m'
+B_White='\033[0;97m'
+Bold='\033[1m'
+Reset='\033[0m'
+
 # Makefile에서 정의한 프로젝트 이름을 읽어오는 함수
 function read_makefile_name() {
     local name_line=$(grep -E '^NAME\s*=' Makefile)
@@ -29,18 +49,18 @@ function read_makefile_flags() {
 
 # 컴파일 함수
 function compile_file() {
-    echo "Compiling..." >&2
+    echo -e "${Blue}Compiling...${Blue}" >&2
     if make all -j; then
         local name_line=$(grep -E '^NAME\s*=' Makefile)
         NAME=$(echo "$name_line" | sed -E 's/^NAME\s*=\s*//; s/^[[:space:]]+//')
         THIRD=$(echo "$NAME" | awk '{print $3}')
         if [ -f "$THIRD" ]; then
-            echo "Compile succeeded!" >&2
+            echo -e "${Green}${Bold}Compile succeeded ! ${Reset}" >&2
         else
-            echo "Compile failed: Executable '$THIRD' not found!" >&2
+            echo "${B_Red}${Bold}Compile failed: Executable '$THIRD' not found!" >&2
         fi
     else
-        echo "Compile failed: Make returned non-zero exit code!" >&2
+        echo "${B_Red}${Bold}Compile failed: Make returned non-zero exit code!${Reset}" >&2
     fi
 }
 
