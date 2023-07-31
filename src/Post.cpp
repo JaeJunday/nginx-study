aa82060415ef89338d01f9fa1963fb5d9769d985
JaeJunday <wowns8114@gmail.com>
2023-07-31 13:53:25 +0900

    Feat: Post response add

---
+++ src/Post.cpp
@@ -0,0 +1,15 @@
+#include "Post.hpp"
+#include "Request.hpp"
+
+Post::Post(Request* request) : AResponse()
+{
+    _request = request;
+}
+
+void Post::createResponseHeader(); // override
+{
+    _buffer << _version << " " << _stateCode << " " << _reasonPhrase << "\r\n";
+	_buffer << "Date: " << getDate() << "\r\n";
+	_buffer << "Server: " << _serverName << "\r\n";
+	_buffer << "Content-Type: " << _contentType << "\r\n";
+}
