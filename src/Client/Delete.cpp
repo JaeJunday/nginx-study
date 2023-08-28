51ffaf82222fdde1ac3b39a7b4d5a3c154eceb8d
JaeJunday <wowns8114@gmail.com>
2023-08-28 20:49:40 +0900

    Fix: delete method response fix

--- src/Client/Delete.cpp
+++ src/Client/Delete.cpp
@@ -2,19 +2,20 @@
 
 void Client::handleDelete()
 {
-    _responseBuffer << _version << " " << _stateCode << " " << _reasonPhrase << "\r\n";
+	_responseBuffer << _version << " " << _stateCode << " " << _reasonPhrase << "\r\n";
 	_responseBuffer << "Date: " << util::getDate() << "\r\n";
 	_responseBuffer << "Server: " << _serverName << "\r\n";
-	_responseBuffer << "Content-Type: " << _contentType << "\r\n";
-    _responseStr = _responseBuffer.str();
-    removeFile(_request->getConvertRequestPath());
-    deleteSocketReadEvent();
-    addSocketWriteEvent();
+	_responseBuffer << "Content-Type: " << _contentType << "\r\n\r\n";
+	_responseStr = _responseBuffer.str();
+	removeFile(_request->getConvertRequestPath());
+	deleteSocketReadEvent();
+	addSocketWriteEvent();
 }
 
 void Client::removeFile(std::string file) const
 {
-    file = "." + file;
-    std::cerr << "file: " << file << std::endl;
-    std::remove(file.c_str());
+	file = "." + file;
+	int isRemove = std::remove(file.c_str());	
+	if (isRemove == -1)
+		throw 404;
 }
